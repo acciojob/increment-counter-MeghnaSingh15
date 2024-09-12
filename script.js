1bd3dba407468fd3e5e03a59115dbaf143632b53
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-12 12:20:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let count =0;
+let para = document.querySelector("p");
+let btn =  document.querySelector("button");
+btn.addEventListener("click", function(){
+	count++;
+	alert(para.innerText);
+	
+	
+})
+
